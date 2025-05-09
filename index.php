d0982d97e37eb254380b3bf55bc2b96d04388ec7
EmersonLima025 <mersin.ptec@gmail.com>
2025-05-09 20:19:37 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
+<!DOCTYPE html>
+  <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width = device=width, initial - scale = 10">
+        <title>Dr. Tec</title>
+        <link rel="stylesheck" type="text/css" href="style.css" media="screan"/>
 
+    </head>
+    <body>
+        <header>
+        </header> <main>
+        <footer> 
+        </footer> </main>
+    </body>
+  </html>
+
+</html>
